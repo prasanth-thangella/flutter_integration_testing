a700cb409cccad677008ac3d7eb5e78981450819
prasanth_thangella <prasanththangella.56@gmail.com>
2022-03-08 18:03:03 +0530

    comit

--- android/app/src/main/kotlin/com/example/flutter_integration_testing/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_integration_testing/MainActivity.kt
@@ -1,14 +1,6 @@
-package com.example.flutter_integration_testing;
+package com.example.flutter_integration_testing
 
-import androidx.test.rule.ActivityTestRule;
-import dev.flutter.plugins.integration_test.FlutterTestRunner;
-import org.junit.Rule;
-import org.junit.runner.RunWith;
-// Import MainActivity from YOUR package
-import com.example.flutter_integration_testing.MainActivity;
+import io.flutter.embedding.android.FlutterActivity
 
-@RunWith(FlutterTestRunner.class)
-public class MainActivityTest {
-  @Rule
-  public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
+class MainActivity: FlutterActivity() {
 }
